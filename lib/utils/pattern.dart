f8b7970f38ff99f96aca47b94de053346e9b186c
Luis <josuepanzu8@gmail.com>
2025-05-17 22:47:48 +0100

    Pattern for email verification

---
+++ lib/utils/pattern.dart
@@ -0,0 +1,3 @@
+const String emailPattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+const String passwordPattern =
+    r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'; // At least 8 characters, 1 letter and 1 number
