cb36ef2aa42d0acfb272f5ca58bd7990392cf8ee
Luis <josuepanzu8@gmail.com>
2025-05-18 15:01:23 +0100

    configuration of login and signup url

--- lib/utils/urlpatterns.dart
+++ lib/utils/urlpatterns.dart
@@ -4,18 +4,18 @@ import 'package:yinzo/screens/budget_history_screen.dart';
 import 'package:yinzo/screens/discussions.screen.dart';
 import 'package:yinzo/screens/forgotpassword.screen.dart';
 import 'package:yinzo/screens/main_screen.dart';
-import 'package:yinzo/screens/login.screen.dart';
+import 'package:yinzo/screens/login_screen.dart';
 import 'package:yinzo/screens/openstreetmapscreen/localisation_map.screen.dart';
 import 'package:yinzo/screens/scheduled_visit_screen.dart';
 import 'package:yinzo/screens/settings_screen.dart';
-import 'package:yinzo/screens/signup.screen.dart';
+import 'package:yinzo/screens/signup_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   '/': (_) => MainScreen(),
   '/map': (_) => LocationMapScreen(),
-  '/sinup': (_) => SignUpScreen(),
+  '/signup': (_) => SignupScreen(),
   'reset-password': (_) => ResetPasswordScreen(),
-  '/login': (_) => SignInScreen(),
+  '/login': (_) => LoginScreen(),
   '/forgot-password': (_) => ForgotPasswordScreen(),
   '/discussions': (_) => DiscussionsScreen(),
   '/budget-history': (_) => BudgetHistoryScreen(),
