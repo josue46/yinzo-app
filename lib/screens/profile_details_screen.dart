b3e02f51ba10184ef1b0dff0b89748b588e23303
Luis <josuepanzu8@gmail.com>
2025-05-25 17:43:35 +0100

    Creating the user profile details screen

---
+++ lib/screens/profile_details_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:yinzo/screens/edit_profile_screen.dart';
+
+class ProfileDetailsScreen extends StatelessWidget {
+  const ProfileDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: const Color(0xFF00BF6D),
+        foregroundColor: Colors.white,
+        title: const Text("Profile"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings_outlined),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              const ProfilePic(
+                image: "https://i.postimg.cc/cCsYDjvj/user-2.png",
+              ),
+              Text(
+                "Josué Luis",
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const Divider(height: 16.0 * 2),
+              const Info(infoKey: "Nom d'utilisateur", info: "@josueluis.me"),
+              const Info(infoKey: "Nom", info: "Panzu"),
+              const Info(infoKey: "Prénom", info: "Josué luis"),
+              const Info(infoKey: "Phone", info: "(243) 994872462"),
+              const Info(infoKey: "Adresse mail", info: "demo@mail.com"),
+              const SizedBox(height: 16.0),
+              Align(
+                alignment: Alignment.centerRight,
+                child: SizedBox(
+                  width: 160,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF00BF6D),
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(double.infinity, 48),
+                      shape: const StadiumBorder(),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (_) => EditProfileScreen()),
+                      );
+                    },
+                    child: const Text("Modifier le profile"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    super.key,
+    required this.image,
+    this.isShowPhotoUpload = false,
+    this.imageUploadBtnPress,
+  });
+
+  final String image;
+  final bool isShowPhotoUpload;
+  final VoidCallback? imageUploadBtnPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 16.0),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Theme.of(
+            context,
+          ).textTheme.bodyLarge!.color!.withOpacity(0.08),
+        ),
+      ),
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          CircleAvatar(radius: 50, backgroundImage: NetworkImage(image)),
+          InkWell(
+            onTap: imageUploadBtnPress,
+            child: CircleAvatar(
+              radius: 13,
+              backgroundColor: Theme.of(context).primaryColor,
+              child: const Icon(Icons.add, color: Colors.white, size: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Info extends StatelessWidget {
+  const Info({super.key, required this.infoKey, required this.info});
+
+  final String infoKey, info;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            infoKey,
+            style: TextStyle(
+              color: Theme.of(
+                context,
+              ).textTheme.bodyLarge!.color!.withOpacity(0.8),
+            ),
+          ),
+          Text(info),
+        ],
+      ),
+    );
+  }
+}
