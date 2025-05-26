51daa2c3df19c90e87c6248484b72f11887841dc
Luis <josuepanzu8@gmail.com>
2025-05-26 23:18:28 +0100

    Improvement of Category model.

--- lib/Logements/Models/category.dart
+++ lib/Logements/Models/category.dart
@@ -1,9 +1,10 @@
 class Category {
   String name;
+  String slug;
 
-  Category({required this.name});
+  Category({required this.name, required this.slug});
 
   factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(name: json["name"]);
+    return Category(name: json["name"], slug: json["slug"]);
   }
 }
