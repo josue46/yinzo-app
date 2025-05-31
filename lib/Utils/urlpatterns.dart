06eb18cbe9baa4da909d4942a7d08d43109921f3
Luis <josuepanzu8@gmail.com>
2025-05-31 20:31:34 +0100

    Amélioration des patterns urls

--- lib/Utils/urlpatterns.dart
+++ lib/Utils/urlpatterns.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yinzo/Budgets/Screens/budget_history_screen.dart';
+import 'package:yinzo/Logements/Screens/logement_detail_screen.dart';
+import 'package:yinzo/Logements/Screens/publish_logement_screen.dart';
 import 'package:yinzo/Pages/main_screen.dart';
 import 'package:yinzo/Pages/settings_screen.dart';
 import 'package:yinzo/Users/Screens/login_screen.dart';
@@ -13,6 +15,16 @@ final Map<String, WidgetBuilder> routes = {
   '/login': (_) => LoginScreen(),
   '/budget-history': (_) => BudgetHistoryScreen(),
   '/settings': (_) => SettingsScreen(),
+  '/logementDetails': (context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    return LogementDetailsScreen(logementId: args['logementId']);
+  },
+  '/publish/logement': (context) {
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    return PublishLogementScreen(token: args['token']);
+  },
   // '/forgot-password': (_) => ForgotPasswordScreen(),
   // 'reset-password': (_) => ResetPasswordScreen(),
   // '/schedule/appointment': (_) => ScheduledVisitScreen(),
