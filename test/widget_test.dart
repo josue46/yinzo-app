fd455bc9b75cd76274e724462bc4211a5ba59223
Luis <josuepanzu8@gmail.com>
2025-05-31 20:36:19 +0100

    Ajout de la dépendance jwt_decoder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:yinzo/Users/Provider/auth_provider.dart';
 
 import 'package:yinzo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const YinzoApp());
+    await tester.pumpWidget(YinzoApp(authProvider: AuthProvider()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
