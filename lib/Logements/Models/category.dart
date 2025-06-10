57984f7b576e3d4f75f2159935ef5320f55a90dd
Luis <josuepanzu8@gmail.com>
2025-06-10 03:00:40 +0100

    Added id field

--- lib/Logements/Models/category.dart
+++ lib/Logements/Models/category.dart
@@ -1,10 +1,11 @@
 class Category {
+  String id;
   String name;
   String slug;
 
-  Category({required this.name, required this.slug});
+  Category({required this.id, required this.name, required this.slug});
 
   factory Category.fromJson(Map<String, dynamic> json) {
-    return Category(name: json["name"], slug: json["slug"]);
+    return Category(id: json["id"], name: json["name"], slug: json["slug"]);
   }
 }
